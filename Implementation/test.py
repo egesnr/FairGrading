d9592775b21ff5324309a05f9b38dd4887fb26da
Ege <egesnr1@gmail.com>
2022-12-01 18:19:33 +0300

    coding

--- Implementation/test.py
+++ Implementation/test.py
@@ -16,9 +16,22 @@ with open("Implementation\Grading_Assignment.csv","r") as file:
 '''
 df = pd.read_csv("Implementation\Grading_Assignment.csv")
 print(df.head())
-
+#Column of the first teacher
 teacher1 = df['sc1']
+#Column of the second one
 teacher2 = df['sc2']
+
+#Mean value of teacher 1
+print("mean of teacher 1 = ",df["sc1"].mean())
+
+#Dropped ID Column for d2 dataframe
+df2 = df.drop('ID', inplace=False, axis=1)
+print(df2.head())
+#Calculate mean value of every teacher 
+for i in df2:
+    print("mean of ",i ," is ",df2[i].mean())
+
+#Comparing values of sc1 and sc2 by histogram
 plt.title('Grading Dataset')
 plt.ylabel('Frequency')
 plt.xlabel('Grades')
