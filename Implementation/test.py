479172b9dc75f142ada718731f3631e62367d42e
Ege <egesnr1@gmail.com>
2022-12-04 16:41:52 +0300

    scatter

--- Implementation/test.py
+++ Implementation/test.py
@@ -15,7 +15,7 @@ with open("Implementation\Grading_Assignment.csv","r") as file:
 
 '''
 df = pd.read_csv("Implementation\Grading_Assignment.csv")
-print(df.head())
+print(df)
 #Column of the first teacher
 teacher1 = df['sc1']
 #Column of the second one
@@ -24,18 +24,29 @@ teacher2 = df['sc2']
 #Mean value of teacher 1
 print("mean of teacher 1 = ",df["sc1"].mean())
 
+
+
 #Dropped ID Column for d2 dataframe
 df2 = df.drop('ID', inplace=False, axis=1)
 print(df2.head())
 #Calculate mean value of every teacher 
+means = []
 for i in df2:
     print("mean of ",i ," is ",df2[i].mean())
-
+    means.append(df2[i].mean())
 #Comparing values of sc1 and sc2 by histogram
+'''
 plt.title('Grading Dataset')
 plt.ylabel('Frequency')
 plt.xlabel('Grades')
 plt.hist(teacher1)
 plt.hist(teacher2,color = 'green',alpha = 0.5)
+#plt.show()
+'''
+plt.title("Instructor's grades")
+plt.ylabel("Means")
+plt.xlabel("Instructors")
+x = np.arange(1,26)
+plt.scatter(x,means)
+plt.xticks(np.arange(1,26))
 plt.show()
-
