6942336f2c289eca6845692c8514a6632927e9bd
yWhire <yinan2001@hotmail.com>
2022-12-26 11:26:08 +0300

    Machine learning Model A

--- Implementation/test.py
+++ Implementation/test.py
@@ -1,27 +1,12 @@
-import csv
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
 import random
 import math
 
-'''
-rows = []
-with open("Implementation\Grading_Assignment.csv","r") as file:
-
-    read = csv.reader(file)
-
-    header = next(read)
-
-    for row in read:
-        rows.append(row)
-
-'''
 df = pd.read_csv("Grading_Assignment.csv")
-
-
-# df = pd.read_csv("Implementation\Grading_Assignment.csv")
-
+# Dropped ID Column for d2 dataframe
+df2 = df.drop('ID', inplace=False, axis=1)
 
 def infos():
     # First 10 row of the dataset
@@ -35,10 +20,6 @@ def infos():
 
     # Mean value of teacher 1
     print("mean of teacher 1 = ", df["sc1"].mean())
-
-
-# Dropped ID Column for d2 dataframe
-df2 = df.drop('ID', inplace=False, axis=1)
 
 
 def calculate_mean():
@@ -68,16 +49,6 @@ def plotting():
     plt.scatter(x, calculate_mean)
     plt.xticks(np.arange(1, 26))
     # plt.show()
-
-
-'''
-for i in range(24):
-    for j in range(i+1,25):
-      combinations = df2.iloc[:, [i, j]]
-      commons = combinations.dropna()
-      teacher_x = commons.iloc[0].mean()
-      teacher_y = commons.iloc[1].mean()
-'''
 
 
 def dev():
@@ -238,107 +209,5 @@ def meanCorrelation_train():
 
     return d
 
-
-a = []
-a2 = []
-a3 = []
-a4 = []
-deviation = []
-sum = 0
-sum2 = 0
-sum3 = 0
-sum4 = 0
-x_sum = 0
-x_array = []
-for i in range(25):
-
-    for j in range(i + 1, 25):
-        combinations = df2.iloc[:, [i, j]]
-        commons = combinations.dropna()
-        length = len(commons)
-
-        random_number1 = random.randint(0, length - 1)
-        random_number2 = random.randint(0, length - 1)
-        teacher_x = commons.iloc[random_number1, 0]
-        teacher_y = commons.iloc[random_number1, 1]
-
-        teacher_xx = commons.iloc[:, 0]
-        teacher_yy = commons.iloc[:, 1]
-
-        teacher_xx = teacher_xx.loc[teacher_xx != teacher_x].mean()
-        teacher_yy = teacher_yy.loc[teacher_yy != teacher_y].mean()
-
-        # Theorem 1
-        p = teacher_xx - teacher_x
-        q = teacher_yy - teacher_y
-        sum += abs(p)
-        sum += abs(q)
-        a.append(p)
-        a.append(q)
-        # Bu kullanılacak !!
-        mean_diff = teacher_xx - teacher_yy
-        # düzeltilecek
-        # predict_x = teacher_y + mean_diff
-        # predict_y = teacher_x + mean_diff
-
-        diff = teacher_xx - teacher_yy
-        deviation_x = np.sqrt(np.sum((teacher_x - teacher_xx) ** 2) / length)
-        deviation_y = np.sqrt(np.sum((teacher_y - teacher_yy) ** 2) / length)
-        predict_x = teacher_y + mean_diff
-        predict_y = teacher_x - mean_diff
-        sum2 += abs(predict_x - teacher_x)
-        sum2 += abs(predict_y - teacher_y)
-        a2.append(predict_x)
-        a2.append(predict_y)
-
-        devide_diff = teacher_xx / teacher_yy
-        predict_2x = teacher_y * devide_diff
-        predict_2y = teacher_x * devide_diff
-        sum3 += abs(predict_2x - teacher_x)
-        sum3 += abs(predict_2y - teacher_y)
-        a3.append(predict_2x)
-        a3.append(predict_2y)
-        # Theorem 4 kontrol et
-        if (teacher_x - teacher_xx) > deviation_x:
-            predict_3y = predict_2y
-        else:
-            predict_3y = teacher_yy
-        if (teacher_y - teacher_yy) > deviation_y:
-            predict_3x = predict_2x
-        else:
-            predict_3x = teacher_xx
-        sum4 = abs(predict_3x - teacher_x)
-        sum4 = abs(predict_3y - teacher_y)
-        a4.append(predict_3x)
-        a4.append(predict_3y)
-        '''
-      if abs(teacher_x-teacher_xx)>deviation_x:
-         predict_y = teacher_x + mean_diff + (abs(teacher_x-teacher_xx)-deviation_x)
-      else:
-         predict_y = teacher_x + mean_diff - (abs(teacher_x-teacher_xx)-deviation_x)
-      if  abs(teacher_y-teacher_yy)>deviation_y:
-         predict_x = teacher_y + mean_diff + (abs(teacher_y-teacher_yy)-deviation_y)
-      else:
-         predict_x = teacher_y + mean_diff - + (abs(teacher_y-teacher_yy)-deviation_y)
-    
-      value_one = predict_x - teacher_x
-      value_two = predict_y - teacher_y
-      sum2 += abs(value_one)
-      sum2 += abs(value_two)
-      a2.append(value_one)
-      a2.append(value_two)
-      '''
-
-# print("len a = ", len(a))
-# print(sum)
-print("Avarage error rate is ", (sum / len(a)))
-print("Avarage error rate for theorem 2 is ", sum2 / len(a2))
-print("Avarage error rate for theorem 3 is ", sum3 / len(a3))
-print("Avarage error rate for theorem 4 is ", sum4 / len(a4))
-# print("Avarage percentage error ",(sum2/len(a2)))
-
-
-# dev()
-# meanCorrelation_train()
 
 multi_split(20)
