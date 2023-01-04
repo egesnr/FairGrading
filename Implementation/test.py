c549e83592c46263789d753d0780bb80016222ab
yWhire <yinan2001@hotmail.com>
2023-01-05 00:42:22 +0300

    Corr

--- Implementation/test.py
+++ Implementation/test.py
@@ -1,6 +1,7 @@
 import csv
 from collections import OrderedDict
 import statistics
+
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
@@ -19,10 +20,10 @@ with open("Implementation\Grading_Assignment.csv","r") as file:
         rows.append(row)
 
 '''
-#df = pd.read_csv("Grading_Assignment.csv")
+df = pd.read_csv("Grading_Assignment.csv")
 
 
-df = pd.read_csv("Implementation\Grading_Assignment.csv")
+# df = pd.read_csv("Implementation\Grading_Assignment.csv")
 
 
 def infos():
@@ -122,6 +123,7 @@ def meanCorrelation(train_data):
 # returns predictions
 
 def MLModelA_simple(inter_data, sample_data):
+    corr_table = corelation_table(df2)
     for i in range(len(sample_data)):
         temp = []
         unknown_temp = []
@@ -135,9 +137,11 @@ def MLModelA_simple(inter_data, sample_data):
             tempSum = 0
             for j in range(len(temp)):
                 if pd.notna(inter_data[temp[j]][unknown_temp[b]]):
-                    corr = inter_data[temp[j]][unknown_temp[b]]
-                    tempSum += sample_data[i][temp[j]]
-                    tempSum += corr
+                    if corr_table[temp[j]][unknown_temp[b]] >= 0.55:
+                        corr = inter_data[temp[j]][unknown_temp[b]]
+                        tempSum += sample_data[i][temp[j]]
+                        tempSum += corr
+
                 else:
                     continue
             average = tempSum / len(temp)
@@ -158,12 +162,13 @@ def find_frequency(data):
     for i in range(len(data.columns)):
         column = data.iloc[:, [i]]
         column = column.dropna()
-        #mySum += len(column)
+        # mySum += len(column)
         insGradeLen.append(len(column))
-    #insGradeLen = [x / mySum for x in insGradeLen]
+    # insGradeLen = [x / mySum for x in insGradeLen]
     return insGradeLen
 
-#Find how many common grade for each pair of teacher 
+
+# Find how many common grade for each pair of teacher
 def common_grade_table(data):
     freq = pd.DataFrame()
     freq2 = []
@@ -171,22 +176,23 @@ def common_grade_table(data):
         for j in range(25):
             combinations = data.iloc[:, [i, j]]
             commons = combinations.dropna()
-            freq2.append(len(commons.iloc[:,0]))
+            freq2.append(len(commons.iloc[:, 0]))
     b = np.array(freq2)
     c = b.reshape(25, 25)
     d = pd.DataFrame(c)
     return d
-     
+
 
 # weighted randomized splitting data train and test by looking the frequency
 # Returns trains 2d array and test dictionary source and grade as key and value correspondingly
-def split_data(data):
+def split_data(data, x):
+    global var
     train_data = data.to_numpy().copy()
     freq = find_frequency(data)
     freq = pd.DataFrame(freq)
-    #print(freq)
+    # print(freq)
     commons = common_grade_table(df2)
-    #print(commons)
+    # print(commons)
     locations = [[]]
     test_dict = [{}]
 
@@ -196,7 +202,7 @@ def split_data(data):
                 locations[i].append(j)
         locations.append([])
     locations = locations[:-1]
-    
+
     for i in range(len(locations)):
         teacher_1 = locations[i][0]
         teacher_2 = locations[i][1]
@@ -205,74 +211,72 @@ def split_data(data):
         freq_teacher_1 = freq.iloc[locations[i][0]][0]
         freq_teacher_2 = freq.iloc[locations[i][1]][0]
         freq_teacher_3 = freq.iloc[locations[i][2]][0]
-        
-        
-        
-        commons_per_project =sorted( [int(commons[teacher_1][teacher_2]),int(commons[teacher_1][teacher_3]),int(commons[teacher_2][teacher_3])])
-        
-        commons_per_project2 = {0 : int(commons[teacher_1][teacher_2]),1 : int(commons[teacher_1][teacher_3]),2 :int(commons[teacher_2][teacher_3])
-       }
+
+        commons_per_project = sorted([int(commons[teacher_1][teacher_2]), int(commons[teacher_1][teacher_3]),
+                                      int(commons[teacher_2][teacher_3])])
+
+        commons_per_project2 = {0: int(commons[teacher_1][teacher_2]), 1: int(commons[teacher_1][teacher_3]),
+                                2: int(commons[teacher_2][teacher_3])
+                                }
+        # Sorting dict
+
         commons_per_project2 = {k: v for k, v in sorted(commons_per_project2.items(), key=lambda item: item[1])}
 
-        
-        
-        
+        # get last item which has max value pair
         if list(commons_per_project2)[-1] == 0:
-           
-           #print("First is selected")
-           if commons[teacher_1][teacher_2] >= 10:
-             #print(freq_teacher_1,freq_teacher_2)
-             if freq_teacher_1 >= freq_teacher_2:
-                var = teacher_1 
-                freq.iloc[locations[i][0]][0] =  freq_teacher_1 - 1 
-             else:
-                var = teacher_2
-                freq.iloc[locations[i][1]][0] =  freq_teacher_2 - 1 
-             commons[teacher_1][teacher_2]-=1
-             commons[teacher_2][teacher_1] -=1
-           
+
+            # print("First is selected")
+            if commons[teacher_1][teacher_2] >= x:
+
+                # print(freq_teacher_1,freq_teacher_2)
+                if freq_teacher_1 >= freq_teacher_2:
+                    var = teacher_1
+                    freq.iloc[locations[i][0]][0] = freq_teacher_1 - 1
+                else:
+                    var = teacher_2
+                    freq.iloc[locations[i][1]][0] = freq_teacher_2 - 1
+                commons[teacher_1][teacher_2] -= 1
+                commons[teacher_2][teacher_1] -= 1
+
         elif list(commons_per_project2)[-1] == 1:
-            
-            #print("second is selected")
-            if commons[teacher_1][teacher_3] >= 10:
-             #print(freq_teacher_1,freq_teacher_3)
-             if freq_teacher_1 >= freq_teacher_3:
-                var = teacher_1
-                freq.iloc[locations[i][0]][0] =  freq_teacher_1 - 1 
-             else:
-                var = teacher_3
-                freq.iloc[locations[i][2]][0] =  freq_teacher_3 - 1 
-             commons[teacher_1][teacher_3]-=1
-             commons[teacher_3][teacher_1] -=1
-           
+
+            # print("second is selected")
+            if commons[teacher_1][teacher_3] >= x:
+                # print(freq_teacher_1,freq_teacher_3)
+                if freq_teacher_1 >= freq_teacher_3:
+                    var = teacher_1
+                    freq.iloc[locations[i][0]][0] = freq_teacher_1 - 1
+                else:
+                    var = teacher_3
+                    freq.iloc[locations[i][2]][0] = freq_teacher_3 - 1
+                commons[teacher_1][teacher_3] -= 1
+                commons[teacher_3][teacher_1] -= 1
+
 
         elif list(commons_per_project2)[-1] == 2:
-            #print("Third is selected")
-            
-            if commons[teacher_2][teacher_3] >= 10:
-             #print(freq_teacher_2,freq_teacher_3)
-             if freq_teacher_2 >= freq_teacher_3:
-                freq.iloc[locations[i][1]][0] =  freq_teacher_2 - 1 
-                var = teacher_2 
-             else:
-                var = teacher_3
-                freq.iloc[locations[i][2]][0]  = freq_teacher_3 - 1 
-             commons[teacher_2][teacher_3]-=1
-             commons[teacher_3][teacher_2]-=1
-            
-             
-             #var[-1] -= 1
-        
+            # print("Third is selected")
+
+            if commons[teacher_2][teacher_3] >= x:
+                # print(freq_teacher_2,freq_teacher_3)
+                if freq_teacher_2 >= freq_teacher_3:
+                    freq.iloc[locations[i][1]][0] = freq_teacher_2 - 1
+                    var = teacher_2
+                else:
+                    var = teacher_3
+                    freq.iloc[locations[i][2]][0] = freq_teacher_3 - 1
+                commons[teacher_2][teacher_3] -= 1
+                commons[teacher_3][teacher_2] -= 1
+
+                # var[-1] -= 1
+
         test_dict[i] = {var: train_data[i, var]}
-        
+
         test_dict.append({})
-        #print("project ",i,"  ",test_dict[i])
+        # print("project ",i,"  ",test_dict[i])
         train_data[i][var] = None
-        
-    #print(freq)    
-        
-        
-        
+
+    # print(freq)
+
     '''
         var = random.choices(locations[i], weights=np.concatenate(freq.iloc[locations[i]].to_numpy()))
         no = var[0]
@@ -281,12 +285,11 @@ def split_data(data):
         test_dict.append(dict())
     '''
     test_dict = test_dict[:-1]
-    
-    #print(test_dict)
-    #print(commons)
+
+    # print(test_dict)
+    # print(commons)
     return train_data, test_dict
 
-    
 
 # calculates error array, root mean square error
 def validation(predictions, test_data):
@@ -294,32 +297,34 @@ def validation(predictions, test_data):
     rmse = 0.0
     for i in range(len(test_data)):
         for key, value in test_data[i].items():
-             if np.isnan(value) == False:
-              
-              rmse += ((predictions[i][key] - value) ** 2)
-              error.append(abs(predictions[i][key] - value))
-             
-             
-    print("Length ",len(error))
+            if np.isnan(value) == False:
+                rmse += ((predictions[i][key] - value) ** 2)
+
+                error.append(abs(predictions[i][key] - value))
+
+    print("Length ", len(error))
+    # max_value = max(error)
+    # print([index for index, item in enumerate(error) if item == max_value])
     rmse = math.sqrt(rmse / len(test_data))
     return error, rmse
 
 
-def multi_split(k):
+def multi_split(a, b):
     error = 0.0
     RMSE = 0.0
-    for i in range(k):
-        train, test1 = split_data(df2)
+    for i in range(a, b):
+        train, test1 = split_data(df2, i)
         pre = MLModelA_simple(meanCorrelation(train), train)
         err, r = validation(pre, test1)
+        print("This is for "+str(i))
         print("Here is the mean of error rate: {0:.2f} [Max: {1:.2f}, Min: {2:.2f}]".format(np.mean(err), np.max(err),
                                                                                             np.min(err)))
         print("RMSE: " + str(r))
         print()
         error += np.mean(err)
         RMSE += r
-    error = error / k
-    RMSE = RMSE / k
+    error = error / (b - a)
+    RMSE = RMSE / (b - a)
     print("Here is the error mean {0:.2f} : ".format(error))
     print("RMSE : {0:.2f} ".format(RMSE))
 
@@ -341,124 +346,144 @@ def meanCorrelation_train():
     d = pd.DataFrame(c)
 
     return d
+
+
 def standart_deviation_table(data):
     standart_deviation_table = []
     mySum = 0
     for i in range(25):
-      for j in range(25):
+        for j in range(25):
+            combinations = data.iloc[:, [i, j]]
+            combinations_dropped = combinations.dropna()
+            first_column = combinations_dropped.iloc[:, 0]
+            second_column = combinations_dropped.iloc[:, 1]
+            column_diffrence = first_column - second_column
+            # population standard deviation
+            standart_deviation_table.append(statistics.pstdev(column_diffrence))
 
-        combinations = data.iloc[:, [i, j]]
-        combinations_dropped= combinations.dropna()
-        first_column = combinations_dropped.iloc[:,0]
-        second_column = combinations_dropped.iloc[:,1]
-        column_diffrence = first_column - second_column
-        standart_deviation_table.append( statistics.stdev(column_diffrence) )
     standart_deviation_table = np.array(standart_deviation_table)
-    standart_deviation_table = standart_deviation_table.reshape(25,25)
+    standart_deviation_table = standart_deviation_table.reshape(25, 25)
     return pd.DataFrame(standart_deviation_table)
-        
-   
-    return standart_deviation_table
-a2 = []
-a3 = []
-a4 = []
-deviation = []
-sum = 0
-sum2 = 0
-sum3 = 0
-sum4 = 0
-x_sum = 0
-x_array = []
-for i in range(25):
 
-    for j in range(i + 1, 25):
-        combinations = df2.iloc[:, [i, j]]
-        commons = combinations.dropna()
-        length = len(commons)
 
-        random_number1 = random.randint(0, length - 1)
-        random_number2 = random.randint(0, length - 1)
-        teacher_x = commons.iloc[random_number1, 0]
-        teacher_y = commons.iloc[random_number1, 1]
+def corelation_table(data):
+    corr_table = []
+    mySum = 0
+    for i in range(25):
+        for j in range(25):
+            combinations = data.iloc[:, [i, j]]
+            combinations_dropped = combinations.dropna()
+            first_column = combinations_dropped.iloc[:, 0]
+            second_column = combinations_dropped.iloc[:, 1]
+            column_diffrence = first_column - second_column
 
-        teacher_xx = commons.iloc[:, 0]
-        teacher_yy = commons.iloc[:, 1]
+            corr_table.append(statistics.correlation(first_column, second_column))
 
-        teacher_xx = teacher_xx.loc[teacher_xx != teacher_x].mean()
-        teacher_yy = teacher_yy.loc[teacher_yy != teacher_y].mean()
+    corr_table = np.array(corr_table)
+    corr_table = corr_table.reshape(25, 25)
+    return pd.DataFrame(corr_table)
 
-        # Theorem 1
-        p = teacher_xx - teacher_x
-        q = teacher_yy - teacher_y
-        sum += abs(p)
-        sum += abs(q)
-        #a.append(p)
-        #a.append(q)
-        # Bu kullanılacak !!
-        mean_diff = teacher_xx - teacher_yy
-        # düzeltilecek
-        # predict_x = teacher_y + mean_diff
-        # predict_y = teacher_x + mean_diff
 
-        diff = teacher_xx - teacher_yy
-        deviation_x = np.sqrt(np.sum((teacher_x - teacher_xx) ** 2) / length)
-        deviation_y = np.sqrt(np.sum((teacher_y - teacher_yy) ** 2) / length)
-        predict_x = teacher_y + mean_diff
-        predict_y = teacher_x - mean_diff
-        sum2 += abs(predict_x - teacher_x)
-        sum2 += abs(predict_y - teacher_y)
-        a2.append(predict_x)
-        a2.append(predict_y)
-
-        devide_diff = teacher_xx / teacher_yy
-        predict_2x = teacher_y * devide_diff
-        predict_2y = teacher_x * devide_diff
-        sum3 += abs(predict_2x - teacher_x)
-        sum3 += abs(predict_2y - teacher_y)
-        a3.append(predict_2x)
-        a3.append(predict_2y)
-        # Theorem 4 kontrol et
-        if (teacher_x - teacher_xx) > deviation_x:
-            predict_3y = predict_2y
-        else:
-            predict_3y = teacher_yy
-        if (teacher_y - teacher_yy) > deviation_y:
-            predict_3x = predict_2x
-        else:
-            predict_3x = teacher_xx
-        sum4 = abs(predict_3x - teacher_x)
-        sum4 = abs(predict_3y - teacher_y)
-        a4.append(predict_3x)
-        a4.append(predict_3y)
-        '''
-      if abs(teacher_x-teacher_xx)>deviation_x:
-         predict_y = teacher_x + mean_diff + (abs(teacher_x-teacher_xx)-deviation_x)
-      else:
-         predict_y = teacher_x + mean_diff - (abs(teacher_x-teacher_xx)-deviation_x)
-      if  abs(teacher_y-teacher_yy)>deviation_y:
-         predict_x = teacher_y + mean_diff + (abs(teacher_y-teacher_yy)-deviation_y)
-      else:
-         predict_x = teacher_y + mean_diff - + (abs(teacher_y-teacher_yy)-deviation_y)
-    
-      value_one = predict_x - teacher_x
-      value_two = predict_y - teacher_y
-      sum2 += abs(value_one)
-      sum2 += abs(value_two)
-      a2.append(value_one)
-      a2.append(value_two)
-      '''
+# a2 = []
+# a3 = []
+# a4 = []
+# deviation = []
+# sum = 0
+# sum2 = 0
+# sum3 = 0
+# sum4 = 0
+# x_sum = 0
+# x_array = []
+# for i in range(25):
+#
+#     for j in range(i + 1, 25):
+#         combinations = df2.iloc[:, [i, j]]
+#         commons = combinations.dropna()
+#         length = len(commons)
+#
+#         random_number1 = random.randint(0, length - 1)
+#         random_number2 = random.randint(0, length - 1)
+#         teacher_x = commons.iloc[random_number1, 0]
+#         teacher_y = commons.iloc[random_number1, 1]
+#
+#         teacher_xx = commons.iloc[:, 0]
+#         teacher_yy = commons.iloc[:, 1]
+#
+#         teacher_xx = teacher_xx.loc[teacher_xx != teacher_x].mean()
+#         teacher_yy = teacher_yy.loc[teacher_yy != teacher_y].mean()
+#
+#         # Theorem 1
+#         p = teacher_xx - teacher_x
+#         q = teacher_yy - teacher_y
+#         sum += abs(p)
+#         sum += abs(q)
+#         a.append(p)
+#         a.append(q)
+#         # Bu kullanılacak !!
+#         mean_diff = teacher_xx - teacher_yy
+#         # düzeltilecek
+#         # predict_x = teacher_y + mean_diff
+#         # predict_y = teacher_x + mean_diff
+#
+#         diff = teacher_xx - teacher_yy
+#         deviation_x = np.sqrt(np.sum((teacher_x - teacher_xx) ** 2) / length)
+#         deviation_y = np.sqrt(np.sum((teacher_y - teacher_yy) ** 2) / length)
+#         predict_x = teacher_y + mean_diff
+#         predict_y = teacher_x - mean_diff
+#         sum2 += abs(predict_x - teacher_x)
+#         sum2 += abs(predict_y - teacher_y)
+#         a2.append(predict_x)
+#         a2.append(predict_y)
+#
+#         devide_diff = teacher_xx / teacher_yy
+#         predict_2x = teacher_y * devide_diff
+#         predict_2y = teacher_x * devide_diff
+#         sum3 += abs(predict_2x - teacher_x)
+#         sum3 += abs(predict_2y - teacher_y)
+#         a3.append(predict_2x)
+#         a3.append(predict_2y)
+#         # Theorem 4 kontrol et
+#         if (teacher_x - teacher_xx) > deviation_x:
+#             predict_3y = predict_2y
+#         else:
+#             predict_3y = teacher_yy
+#         if (teacher_y - teacher_yy) > deviation_y:
+#             predict_3x = predict_2x
+#         else:
+#             predict_3x = teacher_xx
+#         sum4 = abs(predict_3x - teacher_x)
+#         sum4 = abs(predict_3y - teacher_y)
+#         a4.append(predict_3x)
+#         a4.append(predict_3y)
+#         '''
+#       if abs(teacher_x-teacher_xx)>deviation_x:
+#          predict_y = teacher_x + mean_diff + (abs(teacher_x-teacher_xx)-deviation_x)
+#       else:
+#          predict_y = teacher_x + mean_diff - (abs(teacher_x-teacher_xx)-deviation_x)
+#       if  abs(teacher_y-teacher_yy)>deviation_y:
+#          predict_x = teacher_y + mean_diff + (abs(teacher_y-teacher_yy)-deviation_y)
+#       else:
+#          predict_x = teacher_y + mean_diff - + (abs(teacher_y-teacher_yy)-deviation_y)
+#
+#       value_one = predict_x - teacher_x
+#       value_two = predict_y - teacher_y
+#       sum2 += abs(value_one)
+#       sum2 += abs(value_two)
+#       a2.append(value_one)
+#       a2.append(value_two)
+#       '''
 
 # print("len a = ", len(a))
 # print(sum)
-#print("Avarage error rate is ", (sum / len(a)))
-print("Avarage error rate for theorem 2 is ", sum2 / len(a2))
-print("Avarage error rate for theorem 3 is ", sum3 / len(a3))
-print("Avarage error rate for theorem 4 is ", sum4 / len(a4))
+# print("Avarage error rate is ", (sum / len(a)))
+# print("Avarage error rate for theorem 2 is ", sum2 / len(a2))
+# print("Avarage error rate for theorem 3 is ", sum3 / len(a3))
+# print("Avarage error rate for theorem 4 is ", sum4 / len(a4))
 # print("Avarage percentage error ",(sum2/len(a2)))
 
 
 # dev()
 # meanCorrelation_train()
-#print(common_grade_table(df2))
-#multi_split(1)
-print(standart_deviation_table(df2))
+# print(common_grade_table(df2))
+multi_split(8, 11)
+# print(standart_deviation_table(df2))
